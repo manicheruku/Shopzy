0beefef168773eb5bea4b11631301feb10eb61b8
manicheruku <manicheruku4945@gmail.com>
2024-10-10 13:07:49 +0530

    update

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -5,13 +5,12 @@ function Ads() {
   return (
     <>
       <section className="my-20 mx-4 lg:ml-10 lg:mr-10">
-        <div className="w-full flex justify-around  bg-[#045641] h-40 gap-2 px-8 py-4">
-          <div className="flex flex-col  gap-2">
-            <div className="flex items-center  text-white">
+        <div className="w-full flex justify-around bg-[#045641] h-40 gap-2 px-8 py-4 flex-nowrap">
+          <div className="flex flex-col gap-2 min-w-[200px]">
+            <div className="flex items-center text-white">
               <VscCoverage />
-              <p className="text-xs ">AUTHENTICITY GUARANTEE</p>
+              <p className="text-xs">AUTHENTICITY GUARANTEE</p>
             </div>
-
             <p className="font-bold text-white lg:text-3xl text-xl">
               Shop top brands for under 2000
             </p>
@@ -22,7 +21,7 @@ function Ads() {
               Jump in
             </button>
           </div>
-          <div className="flex sm:gap-20 gap-4">
+          <div className="flex gap-4 items-center flex-nowrap">
             <img src="shoes.png" className="lg:w-32 sm:w-20 w-10" alt="" />
             <img src="t-shirt.png" className="lg:w-32 sm:w-20 w-10" alt="" />
           </div>
