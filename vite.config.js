9ca4891cbf1accc4508f16a25504d5bb79cff2b0
manicheruku <manicheruku4945@gmail.com>
2024-10-10 12:01:46 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Shopzy/",
   plugins: [react()],
-  build: {
-    outDir: "dist", // Change this to 'build'
-  },
 });
